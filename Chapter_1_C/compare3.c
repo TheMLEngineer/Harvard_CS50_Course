2935bfe3bfa1a2df5170b1b74a4fd128bf37c558
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2024-10-16 00:52:19 +0530

    Chapter 1 , C

---
+++ Chapter_1_C/compare3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "cs50.h"
+
+int main(void)
+{
+    int x = get_int("What's X : ");
+    int y = get_int("What's Y : ");
+
+    if (x < y)
+    {
+        printf("x is less than y \n");
+    }
+    else if (x > y)
+    {
+        printf("x is greater than y \n");
+    }
+    else if (x==y)
+    {
+        printf("x is equal to y \n");
+    }
+
+}

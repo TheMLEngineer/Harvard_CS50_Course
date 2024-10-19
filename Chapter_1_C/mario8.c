1636fcd87060338d323f2b2cf3b040f0f7e4299c
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2024-10-20 01:16:03 +0530

    Chapter 1 , C

---
+++ Chapter_1_C/mario8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "cs50.h"
+
+int main(void)
+{
+    int n = get_int("Square size : ");
+    while (n < 1)
+    {
+        n = get_int("Square size : ");
+    }
+    for (int i = 0;i < n; i++)
+    {
+        for (int j = 0;j < n; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}

1ce4195c998341952406bea6e70ae3e953c204af
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2024-10-20 19:12:49 +0530

    l

---
+++ Chapter_1_C/learn_patterns5.c
@@ -0,0 +1,20 @@
+#include "cs50.h"
+#include <stdio.h>
+
+int main(void)
+{
+    int row ;
+    do
+    {
+        row  = get_int("Height : ");
+    }
+    while (row<1 || row>8);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0;j<=i;j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}

aa57d070859f42e0d35c4382b2b2839ff705227b
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2024-10-20 22:09:48 +0530

    learn

--- Chapter_1_C/learn_patterns6.c
+++ Chapter_1_C/learn_patterns6.c
@@ -1,6 +1,37 @@
 #include "cs50.h"
 #include <stdio.h>
 
+
+/*
+
+# Print J
+
+
+PS C:\Users\KARTHIKEYAN MUTHU\Documents\Harvard CS50\Harvard_CS50_Course\Chapter_1_C> .\learn_patterns6.exe
+Height : 4
+0
+01
+012
+0123
+PS C:\Users\KARTHIKEYAN MUTHU\Documents\Harvard CS50\Harvard_CS50_Course\Chapter_1_C> 
+
+
+# Print K
+
+
+PS C:\Users\KARTHIKEYAN MUTHU\Documents\Harvard CS50\Harvard_CS50_Course\Chapter_1_C> .\learn_patterns6.exe
+Height : 4
+0123
+012
+01
+0
+PS C:\Users\KARTHIKEYAN MUTHU\Documents\Harvard CS50\Harvard_CS50_Course\Chapter_1_C> 
+
+
+*/
+
+
+
 int main(void)
 {
     int row ;
@@ -13,11 +44,13 @@ int main(void)
     {
         for (int j = 0;j<=i;j++)
         {
+            //printf("%i",j);
             printf(" ");
         }
         for (int k = 0 ; k <= row - (i+1) ; k++ )
         {
-            printf("*",k);
+            //printf("%i",k);
+            printf("*");
         }
         printf("\n");
     }

6ccb837cf6f0c36a137e0700713292ee01b1bc67
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2024-10-28 02:13:29 +0530

    Hi7

---
+++ Chapter_2_Arrays/hi7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string words[2];
+    words[0] = "Hi!";
+    words[1] = "BYE!";
+
+    printf("%c\n",words[0][0]);
+    printf("%c\n",words[1][0]);
+
+}
